981c9b10eb34253b979756b285253180fdccdf7f
isiyoks <64591185+isiyoks@users.noreply.github.com>
2024-04-29 11:11:51 -0400

    303 Practice SBA1 Division

---
+++ src/main/java/SBA/SBA1/Division.java
@@ -0,0 +1,33 @@
+package SBA.SBA1;
+
+import java.util.Scanner;
+
+/*
+Create a  method to divide the digits of a given string.
+Strings length is between 2 - 100;
+
+EX String s = 824
+Answer = 1;
+
+8/2 = 4
+4/2 =1
+ */
+public class Division {
+
+    public static int divideDigits(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write a number that its length is between 2 - 100");
+        String number = scan.nextLine();
+        int result = Character.getNumericValue(number.charAt(0));   // 8/2=4
+
+        for (int i = 1; i < number.length(); i++) {
+            result /= Character.getNumericValue(number.charAt(i)); //  4/2
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("divideDigits() = " + divideDigits());
+    }
+
+}
