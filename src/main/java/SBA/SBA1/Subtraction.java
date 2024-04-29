ecc929a122469d93aed47255e7b15b23546c73f5
isiyoks <64591185+isiyoks@users.noreply.github.com>
2024-04-29 11:12:04 -0400

    303 Practice SBA1 Subtraction

---
+++ src/main/java/SBA/SBA1/Subtraction.java
@@ -0,0 +1,34 @@
+package SBA.SBA1;
+
+import java.util.Scanner;
+
+/*
+Create a  method to subtract the digits of a given string.
+
+Strings length is between 2 - 100;
+
+EX String s = 824   ((digit1 -digit2)- digit3)-digit4
+Answer = 2;
+8-2 = 6
+6-4 =2
+ */
+public class Subtraction {
+
+    public static int subtractDigits() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write a number that its length is between 2 - 100");
+        String number = scan.nextLine();
+
+        int result = Character.getNumericValue(number.charAt(0));
+
+        for (int i = 1; i < number.length(); i++) {
+            result -= Character.getNumericValue(number.charAt(i));   //8-2=6    6-4=2
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int answer = subtractDigits();
+        System.out.println(answer);
+    }
+}
